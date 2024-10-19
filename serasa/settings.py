ec0771b46667ccddaed53f59cb95fd8ce27abfaf
Jotagê Sales <jotage_sales@hotmail.com>
2024-10-19 16:47:30 -0400

    feat: deploy heroku

--- serasa/settings.py
+++ serasa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 from dj_database_url import parse as db_url
 
 
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['*'], cast=list)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(delimiter=','))
 
 # Application definition
 
